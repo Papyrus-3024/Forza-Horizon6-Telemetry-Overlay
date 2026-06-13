70865a52a7b4f8e9ca51e5dce58889dff55d82ff
﻿Papyrus-3024 <kitchenijp@gmail.com>
2026-06-13 13:32:19 -0600

    Add three layout presets

---
+++ src/Fh6.Telemetry.Overlay/Layouts/CenterDashLayout.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace Fh6.Telemetry.Overlay.Layouts;
+
+public partial class CenterDashLayout : UserControl
+{
+    public CenterDashLayout() => InitializeComponent();
+}
