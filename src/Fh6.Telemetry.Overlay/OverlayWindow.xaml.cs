23f3003fc17595722fafa8ebaf920d238b99f55c
﻿Papyrus-3024 <kitchenijp@gmail.com>
2026-06-13 13:26:40 -0600

    Scaffold transparent overlay window project

---
+++ src/Fh6.Telemetry.Overlay/OverlayWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace Fh6.Telemetry.Overlay;
+
+public partial class OverlayWindow : Window
+{
+    public OverlayWindow()
+    {
+        InitializeComponent();
+    }
+}
