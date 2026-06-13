7f33b325107774b481941e9a851abc8cd68f6a91
﻿Papyrus-3024 <kitchenijp@gmail.com>
2026-06-13 13:31:43 -0600

    Add six telemetry widgets

---
+++ src/Fh6.Telemetry.Overlay/Widgets/SpeedWidget.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace Fh6.Telemetry.Overlay.Widgets;
+
+public partial class SpeedWidget : UserControl
+{
+    public SpeedWidget() => InitializeComponent();
+}
