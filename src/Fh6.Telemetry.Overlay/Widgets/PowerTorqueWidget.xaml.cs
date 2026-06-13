cfb64bde6a2f6b4c681a199c6cb5bd60f9353b0c
﻿Papyrus-3024 <kitchenijp@gmail.com>
2026-06-13 16:39:10 -0600

    feat(overlay): add PowerTorqueWidget showing power (hp) and torque (lb·ft)

---
+++ src/Fh6.Telemetry.Overlay/Widgets/PowerTorqueWidget.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace Fh6.Telemetry.Overlay.Widgets;
+
+public partial class PowerTorqueWidget : UserControl
+{
+    public PowerTorqueWidget() => InitializeComponent();
+}
